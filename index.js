99f3d716855000981289c43d99a45b445488b19a
Swayam Gupta <swayamgupta5705367@gmail.com>
2021-12-01 01:48:08 +0530

    swayamcodes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 
 const app = express()
-
+let ct = 0;
 
 app.use("/api/static", express.static("./static"));
 
@@ -17,6 +17,18 @@ app.get("/api/test", (req, res) => {
     })
 })
 
+app.get("/api/count", (req, res) => {
+    ct++;
+    res.json({
+        success: true,
+        data: {
+            name: "Swayam Gupta",
+            designation: "CEO",
+            company: "swayamcodes",
+            requestCount: ct
+        }
+    })
+})
 
 app.listen(5000, () => {
     console.log("server started at port 5000...")
